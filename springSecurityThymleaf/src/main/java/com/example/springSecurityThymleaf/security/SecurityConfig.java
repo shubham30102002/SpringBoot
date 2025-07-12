26820342f90195d71b285728761eb90f8be30063
Shubham Gupta <sgupta@montran.com>
2025-07-12 19:59:56 +0530

    12/07

--- springSecurityThymleaf/src/main/java/com/example/springSecurityThymleaf/security/SecurityConfig.java
+++ springSecurityThymleaf/src/main/java/com/example/springSecurityThymleaf/security/SecurityConfig.java
@@ -6,34 +6,53 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
 
-        UserDetails shubham = User.builder()
-                .username("shubham")
-                .password("{noop}pass123")
-                .roles("ADMIN","MANAGER","EMPLOYEE")
-                .build();
+        //query to fetch the user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select username, password, enabled from auth_users where username=?");
 
-        UserDetails yash = User.builder()
-                .username("yash")
-                .password("{noop}yash123")
-                .roles("EMPLOYEE")
-                .build();
+        //query to fetch the role by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select username, authority from auth_authorities where username=?");
 
-        UserDetails kuber = User.builder()
-                .username("kuber")
-                .password("{noop}pass123")
-                .roles("MANAGER","EMPLOYEE")
-                .build();
-
-        return new InMemoryUserDetailsManager(shubham, yash, kuber);
+        return jdbcUserDetailsManager;
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails shubham = User.builder()
+//                .username("shubham")
+//                .password("{noop}pass123")
+//                .roles("ADMIN","MANAGER","EMPLOYEE")
+//                .build();
+//
+//        UserDetails yash = User.builder()
+//                .username("yash")
+//                .password("{noop}yash123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails kuber = User.builder()
+//                .username("kuber")
+//                .password("{noop}pass123")
+//                .roles("MANAGER","EMPLOYEE")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(shubham, yash, kuber);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
